15b360892cc99fd229adf24d730254f18a54dd76
kumarnaiduadari <64322283+kumarnaiduadari@users.noreply.github.com>
2020-04-30 13:04:55 +0530

    programm for insertion sorting

---
+++ kumar.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int a[5]={5,4,3,2,1},i,j,k,n,max,swap;
+    for(i=1;i<4;i++)
+         {
+            for(j=4;j>=0;j--)
+              {
+                   max=a[0];
+                   for(k=0;k<=j;k++)
+                       if(max<=a[k])
+                           {
+                                max=a[k];
+                                swap=a[j];
+                                a[j]=a[k];
+                                a[k]=swap;
+                          }
+            }
+
+
+        }
+for(n=0;n<5;n++)
+    printf("%d ",a[n]);
+    printf("\n");
+
+}
+ 
